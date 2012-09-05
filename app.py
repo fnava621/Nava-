2f64ba56cea12ca2b528c3feba070a02721b3d07
Fernando Nava <fnava621@gmail.com>
2012-09-04 23:43:41 -0700

    added blog style

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ def home():
 def blog():
   return render_template('ogilvy.html')
 
+@app.route('/hackernews')
+def news():
+  return render_template('HackerNews.html')
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -30,4 +34,4 @@ def page_not_found(error):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
